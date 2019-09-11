9ce78f96654f54ea1d256ffa4a71eda7d719e558
dnnyp <dnnypham@gmail.com>
2019-09-10 20:50:53 -0400

    Bug Fix: Correct URL for production API

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://morning-shore-48477.herokuapp.com/',
+  production: 'https://morning-shore-48477.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
