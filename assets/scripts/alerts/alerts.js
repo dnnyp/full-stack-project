5993e00171d6a87ada76af477007fbbe62a55cfd
dnnyp <dnnypham@gmail.com>
2019-09-12 02:46:19 -0400

    Styling: Navbar margins and padding
    - adjust padding and margin for factory generated alerts
    - adjust margins for navbar brand and navbar buttons

--- assets/scripts/alerts/alerts.js
+++ assets/scripts/alerts/alerts.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const newAlert = (type, message, delay) => {
-  $('#message-container').append(`<div class="alert alert-${type} alert-fade no-select ml-2" role="alert">${message}</div>`)
+  $('#message-container').append(`<div class="alert alert-${type} alert-fade no-select ml-2 mb-0 p-2" role="alert">${message}</div>`)
   $('.alert-fade').delay(delay).fadeOut(2000)
 }
 
